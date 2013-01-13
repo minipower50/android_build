034544c5dfcf501924fc836b5f1d9b1d9b6dcac0
loosethisskin <loosethisskin@gmail.com>
2013-01-12 23:29:53 -0500

    Revert "Added custom kernel 4.6+ toolchain option"
    This reverts commit d76ba1f1901c5f1b7ada07579200c0cdf88b000e.

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -131,13 +131,6 @@ ifeq ($(TARGET_ARCH),arm)
         endif
       endif
     endif
-    ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN_DIR),)
-        ifeq ($(HOST_OS),darwin)
-            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/$(TARGET_ARCH)/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN_DIR)/bin/arm-eabi-"
-        else
-            ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/$(TARGET_ARCH)/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN_DIR)/bin/arm-eabi-"
-        endif
-    else
     ifneq ($(TARGET_KERNEL_CUSTOM_TOOLCHAIN),)
         ifeq ($(HOST_OS),darwin)
             ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilt/darwin-x86/toolchain/$(TARGET_KERNEL_CUSTOM_TOOLCHAIN)/bin/arm-eabi-"
@@ -146,7 +139,6 @@ ifeq ($(TARGET_ARCH),arm)
         endif
     else
         ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
-    endif
     endif
     ccache = 
 endif
